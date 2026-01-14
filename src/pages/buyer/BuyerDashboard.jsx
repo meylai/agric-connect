d212dc49bc46afc7d8e899a96c2e42d7462bff28
MTMphepo <mphepomeylait@gmail.com>
2026-01-14 13:19:21 +0200

    fixed links in buyer dashboard

--- src/pages/buyer/BuyerDashboard.jsx
+++ src/pages/buyer/BuyerDashboard.jsx
@@ -1,5 +1,6 @@
 // pages/buyer/BuyerDashboard.jsx
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import React from 'react';
 import BuyerHome from './BuyerHome';
 import BrowseProduce from './BrowseProduce';
 import Purchases from './Purchases';
